a1c12ac1f70e6de30b8e53cdeb0d0994c822a99d
zpp_sleeping <130629163+czzpp@users.noreply.github.com>
2024-12-10 18:43:17 +0800

    Update judge-mac.sh

--- judge-mac.sh
+++ judge-mac.sh
@@ -1,5 +1,5 @@
-python GetTest.py $2 $4 $5 &\
-g++ $1.cpp -o $1 -std=c++14 -O2 -Wall &\
-./$1 < $2 > $3 &\
-diff $3 $4 > diff.txt &\
-rm $1
+python3 GetTest.py $2 $4 $5 \
+&& g++ $1.cpp -O2 -Wall -std=c++14 -o $1 \
+&& ./$1 < $2 > $3 \
+&& diff $3 $4 > diff.txt \
+&& rm $1
