b06e76c6111ae85b91ce394007f2471a1ff108ad
zpp_sleeping <130629163+czzpp@users.noreply.github.com>
2024-12-13 20:18:35 +0800

    Update GetTest.py

--- GetTest.py
+++ GetTest.py
@@ -1,9 +1,9 @@
 import sys
 n = sys.argv[3]
 if n != "-1":
-    f_in = open(sys.argv[1], 'w', encoding='utf-8') # +".txt"
-    f_out = open(sys.argv[2], 'w', encoding='utf-8')
-    with open("timian.md", 'r', encoding='utf-8') as q:
+    f_in = open(sys.argv[1], 'w') # +".txt"
+    f_out = open(sys.argv[2], 'w')
+    with open("timian.md", 'r') as q:
         line = q.readline()
         while line:
             if "样例 #"+n in line:
@@ -23,9 +23,11 @@ if n != "-1":
                         break
                     f_out.write(line)
                 print("Successfully get Sample Output #"+n)
-                break
+                sys.exit(0)
             line = q.readline()
     f_in.close()
     f_out.close()
+    print("Failed to get Sample #"+n)
+    sys.exit(1)
 else:
     print("Using lagre sample...")
